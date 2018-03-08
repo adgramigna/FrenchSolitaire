2f5d31dc11564d7e858d292586c51b7003b5c423
Andrew D.Gramigna <agramign@cs.umass.edu>
2018-03-08 14:28:08 -0500

    Initial empty spot x and y coordinates must be specified now, makes code simpler

--- FSSim.java
+++ FSSim.java
@@ -2,10 +2,7 @@ public class FSSim{
 	public static void main(String[] args){
 		FSBoard board = new FSBoard(Integer.parseInt(args[1]),Integer.parseInt(args[1]));
 		board.initialize(args[0]);
-		if(args.length > 2)
-			board.makeInitialEmpty(Integer.parseInt(args[2]),Integer.parseInt(args[3]));
-		else
-			board.makeInitialEmpty();
+		board.makeInitialEmpty(Integer.parseInt(args[2]),Integer.parseInt(args[3]));
 		board.play();
 
 	}
